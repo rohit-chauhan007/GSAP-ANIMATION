3ccbb25a4eb103368fe9cf3278877c8c95ecb780
rohit chauhan <rk9331498@gmail.com>
2026-09-09 20:13:12 +0530

    practice question 3

--- PRACTICE1/script.js
+++ PRACTICE1/script.js
@@ -11,3 +11,33 @@ gsap.to(box,{
     repeat:-1,
     ease:"bounce.out"
 })
+const box1 = document.querySelector(".box1");
+
+gsap.fromTo(box1,//from
+    {
+       x:500,
+       rotate:720,
+       borderRadius:20,
+       ease:"bounce.out",
+       delay:2,
+       backgroundColor:"pink"
+    },
+    {
+     y:0,
+     rotate:0,
+     backgroundColor:"yellow",
+     delay:3
+
+    }
+
+);
+//plain javascript object
+const score = {value:0};
+gsap.to(score,{
+  value:500,
+  duration:3,
+  delay:3,
+  onUpdate:()=>{
+    console.log(score);
+  }
+})
