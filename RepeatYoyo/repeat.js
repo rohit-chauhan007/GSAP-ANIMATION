b092b165a318733e2081ba68640ae8c4a620042d
rohit chauhan <rk9331498@gmail.com>
2026-09-09 13:39:37 +0530

    REPEAT & YOYO

---
+++ RepeatYoyo/repeat.js
@@ -0,0 +1,11 @@
+gsap.to(".box",{
+    x:200,
+    y:400,
+    delay:2,
+    duration:2,
+    borderRadius:100,
+    repeat:3,//animation repeat 3 times
+   // repeat:-1,//infinite animation
+    yoyo:true//reverse animation
+
+});
