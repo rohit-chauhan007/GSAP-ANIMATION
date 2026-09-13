9c733436455c6f7a819c100cc5d3a18d796a4250
rohit chauhan <rk9331498@gmail.com>
2026-09-13 18:55:46 +0530

    Stagger properties

--- STAGGER/script.js
+++ STAGGER/script.js
@@ -6,7 +6,7 @@ each - line by line start
 from:"end / center /start / "
 amount -
 */
-
+/*
 gsap.to(".box",{
     x:900,
     duration:1,
@@ -21,3 +21,22 @@ gsap.to(".box",{
         amount:1,
     }
 })
+    */
+   /* gsap.from(".box",{
+    y:50,
+    opacity:0,
+    duration:0.8,
+    stagger:0.20,
+    ease:"power3.out"
+   })
+    */
+
+   //stagger:grid/axis
+   gsap.from(".box",{
+    scale:0.1,
+    duration:0.5,
+    stagger:{
+        grid:"[2,3]",
+        from:"center"
+    }
+   })
