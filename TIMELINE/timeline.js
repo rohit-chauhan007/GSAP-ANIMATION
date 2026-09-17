d5f6b95cf4c220ff21b2f8870d63bf14c593e9c6
rohit chauhan <rk9331498@gmail.com>
2026-09-17 19:55:28 +0530

    NEW PRACTICE QUESTION AND MINI PROJECTS ADDING

--- TIMELINE/timeline.js
+++ TIMELINE/timeline.js
@@ -26,21 +26,48 @@ Previous animation ke end par
 "+=0.5"
 0.5 sec later
 */
-const tl = gsap.timeline();
+const tl = gsap.timeline({pause:true});
 
-tl.to(".box1", {
-  x: 300,
-  duration: 1,
-  delay:1
+// tl.to(".box1", {
+//   x: 300,
+//   duration: 1,
+//   delay:1
+// })
+// .to(".box2", {
+//   x: 300,
+//   duration: 1
+// }, "+=0.5")
+// .to(".box3",{
+//   x:300,
+//   duration:1
+// },"-=0.1")
+
+// tl.pause();
+/*time line label
+tl.addLabel("start")
+.to(".box1",{
+  x:100
 })
-.to(".box2", {
-  x: 300,
-  duration: 1
-}, "+=0.5")
-.to(".box3",{
-  x:300,
-  duration:1
-},"-=0.1")
+.addLabel("middle")
+.to(".box2",{x:100})
+.to(".box3",{opacity:1},"moddle")
+*/
 
+//controll method and playback
+//const tl  = gsap.timelin({pause:true});
+const playBtn = document.querySelector(".play-btn");
+const reverseBtn = document.querySelector(".reverse-btn");
+tl.to(".box1",
+  {
+    x:900,
 
+  }
+)
+tl.pause();
+playBtn.addEventListener("click",()=>{
+  tl.play();
+})
 
+reverseBtn.addEventListener("click",()=>{
+  tl.reverse();
+})
